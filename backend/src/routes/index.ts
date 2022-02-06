9c5648c0eaa056df076486c6c782dbca29e34877
Dominic-Hoe <domhoe2004@gmail.com>
2022-02-06 03:23:52 +0000

    feat: Release v1.0.0

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router, Request, Response, NextFunction } from "express";
 import AuthRouter from "./AuthRouter";
 import SkillsRouter from "./SkillsRouter";
 import ContactsRouter from "./ContactsRouter";
+import ProjectsRouter from "./ProjectsRouter";
 
 const router = Router();
 router.use("/auth", AuthRouter);
 router.use("/skills", SkillsRouter);
 router.use("/contacts", ContactsRouter);
+router.use("/projects", ProjectsRouter);
 
 router.use((_req: Request, res: Response, next: NextFunction) => {
     res.status(400).json({
